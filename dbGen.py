2da85e4dbcedfe467f9ac69501ec8bd0b3ce4cec
Olivier C. Larocque <olivier.c.larocque@gmail.com>
2016-04-27 19:04:56 -0400

    dbGen: Generate messages and signals

--- dbGen.py
+++ dbGen.py
@@ -15,9 +15,18 @@ def generate(board_list):
 
 	for board in board_list:
 		for message in board.message:
-			fo.write("\n\n" + "BO_" + " " + str(message.id) + " " + str(message.name) + ": " + "DLC" + " " + "Vector__XXX" + "\n")
+			dlc = 0
+			signal_offset = 0
 			for signal in message.signal:
-				fo.write(" SG_" + " " + str(signal.name) + " : " + "0|" + str(signal.bitsize) + '@1- (1|0) [2|4] "V" Vector__XXX' + "\n")
+				dlc = dlc + (int(signal.bitsize) / 8)
+			fo.write("\n\n" + "BO_" + " " + str(board.offset + message.id) + " " + str(message.name) + ": " + str(dlc) + " " + "Vector__XXX" + "\n")
+			for signal in message.signal:
+				if (signal.signed == "true"):
+					signed = "-"
+				else:
+					signed = "+"
+				fo.write(" SG_" + " " + str(signal.name) + " : " + str(signal_offset) + "|" + str(signal.bitsize) + "@1" + str(signed) + " (" + str(signal.factor) + "," + str(signal.offset) + ")" + " [" + str(signal.minvalue) + "|" + str(signal.maxvalue) + "]" + " " + '"' + str(signal.unit) + '"' + " " + "Vector__XXX" + "\n")
+				signal_offset = signal_offset + int(signal.bitsize)
 				# ET.SubElement(deviceitem, "unit").text = str(signal.unit)
 				# ET.SubElement(deviceitem, "bitsize").text = str(signal.bitsize)
 				# ET.SubElement(deviceitem, "minvalue").text = str(signal.minvalue)
@@ -27,3 +36,5 @@ def generate(board_list):
 				# ET.SubElement(deviceitem, "offset").text = str(signal.offset)
 				# ET.SubElement(deviceitem, "signed").text = str(signal.signed)
 				# ET.SubElement(deviceitem, "isFloat").text = str(signal.float)
+
+		
